75ff5ede47da34fd32f0129489c041eac3f3f48a
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-08-02 13:38:39 +0300

    Add CommandLineParser package

---
+++ gh-contributor/gh-contributor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gh_contributor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
